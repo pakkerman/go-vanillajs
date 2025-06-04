0ec1cfe756109f08da58a4c978f2a465965026fd
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2025-06-04 13:26:36 +0800

    feat: create file server and index page

--- main.go
+++ main.go
@@ -1,4 +1,16 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 func main() {
+	http.Handle("/", http.FileServer(http.Dir("public")))
+
+	const addr = ":3000"
+	err := http.ListenAndServe(addr, nil)
+	if err != nil {
+		log.Fatalf("Server failed: %v", err)
+	}
 }
