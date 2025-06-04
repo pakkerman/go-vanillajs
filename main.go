9b0574435f454ef075322aa5f4603fd02b7c83fe
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2025-06-04 13:52:51 +0800

    feat: add logger.go

--- main.go
+++ main.go
@@ -3,14 +3,29 @@ package main
 import (
 	"log"
 	"net/http"
+
+	logger "github.com/pakkerman/looger"
 )
 
+func initializeLogger() *logger.Logger {
+	logInstance, err := logger.NewLogger("movie.log")
+	if err != nil {
+		log.Fatalf("Failed to initialize logger %v", err)
+	}
+
+	defer logInstance.Close()
+	return logInstance
+}
+
 func main() {
+	logInstance := initializeLogger()
+
 	http.Handle("/", http.FileServer(http.Dir("public")))
 
 	const addr = ":3000"
 	err := http.ListenAndServe(addr, nil)
 	if err != nil {
 		log.Fatalf("Server failed: %v", err)
+		logInstance.Error("Server failed", err)
 	}
 }
