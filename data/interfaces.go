e2922ecad024919b34ad52f3c843cf64159ed8d8
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2025-06-04 17:18:44 +0800

    chore: add model json tags

---
+++ data/interfaces.go
@@ -0,0 +1,11 @@
+package data
+
+import "github.com/pakkerman/models"
+
+type MovieStroage interface {
+	GetTopMovies() ([]models.Movie, error)
+	GetRandomMovies() ([]models.Movie, error)
+	GetMovieById() (models.Movie, error)
+	SearchMoviesByName(name string) ([]models.Movie, error)
+	GetAllGenres() ([]models.Genre, error)
+}
