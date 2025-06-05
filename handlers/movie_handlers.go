e43dc7177e66fb948fb601a2e63b75b547040ce7
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2025-06-05 11:17:09 +0800

    feat: fetch data from db

--- handlers/movie_handlers.go
+++ handlers/movie_handlers.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/pakkerman/data"
 	"github.com/pakkerman/logger"
-	"github.com/pakkerman/models"
 )
 
 type MovieHandler struct {
@@ -23,48 +22,19 @@ func (h *MovieHandler) writeJsonResponse(w http.ResponseWriter, data interface{}
 }
 
 func (h *MovieHandler) GetTopMovies(w http.ResponseWriter, r *http.Request) {
-	movies := []models.Movie{
-		{
-			ID:          1,
-			TMDB_ID:     181,
-			Title:       "The Hacker",
-			ReleaseYear: 2022,
-			Genres:      []models.Genre{{ID: 1, Name: "Thriller"}},
-			Keywords:    []string{},
-			Casting:     []models.Actor{{ID: 1, FirstName: "Max", LastName: "Huge"}},
-		}, {
-			ID:          2,
-			TMDB_ID:     188,
-			Title:       "Back to the Future",
-			ReleaseYear: 1984,
-			Genres:      []models.Genre{{ID: 1, Name: "Thriller"}},
-			Keywords:    []string{},
-			Casting:     []models.Actor{{ID: 1, FirstName: "Max", LastName: "Huge"}},
-		},
+	movies, err := h.Storage.GetTopMovies()
+	if err != nil {
+		http.Error(w, "Internal Error", 500)
+		h.Logger.Error("GetTopMovies Error: ", err)
 	}
-
 	h.writeJsonResponse(w, movies)
 }
 
 func (h *MovieHandler) GetRandomMovies(w http.ResponseWriter, r *http.Request) {
-	movies := []models.Movie{
-		{
-			ID:          1,
-			TMDB_ID:     181,
-			Title:       "The Hacker",
-			ReleaseYear: 2022,
-			Genres:      []models.Genre{{ID: 1, Name: "Thriller"}},
-			Keywords:    []string{},
-			Casting:     []models.Actor{{ID: 1, FirstName: "Max", LastName: "Huge"}},
-		}, {
-			ID:          2,
-			TMDB_ID:     188,
-			Title:       "Back to the Future Random",
-			ReleaseYear: 1984,
-			Genres:      []models.Genre{{ID: 1, Name: "Thriller"}},
-			Keywords:    []string{},
-			Casting:     []models.Actor{{ID: 1, FirstName: "Max", LastName: "Huge"}},
-		},
+	movies, err := h.Storage.GetRandomMovies()
+	if err != nil {
+		http.Error(w, "Internal Error", 500)
+		h.Logger.Error("GetRandomMovies Error: ", err)
 	}
 
 	h.writeJsonResponse(w, movies)
