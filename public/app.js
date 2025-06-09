78331f72ea373b1c112e6fdfcf5b8ea451ac83b6
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2025-06-09 16:51:03 +0800

    feat: add youtube-embed component

--- public/app.js
+++ public/app.js
@@ -3,10 +3,11 @@ import { API } from "./services/API.js";
 import { HomePage } from "./components/HomePage.js";
 import { MovieDetailsPage } from "./components/MovieDetailsPage.js";
 import "./components/AnimatedLoading.js";
+import "./components/YouTubeEmbed.js";
 
 window.addEventListener("DOMContentLoaded", (event) => {
-  document.querySelector("main").appendChild(new MovieDetailsPage());
   // document.querySelector("main").appendChild(new HomePage());
+  document.querySelector("main").appendChild(new MovieDetailsPage());
 });
 
 window.app = {
--- public/components/MovieDetailsPage.js
+++ public/components/MovieDetailsPage.js
@@ -6,8 +6,7 @@ export class MovieDetailsPage extends HTMLElement {
 
   async render() {
     try {
-      this.movie = await API.getMovieById(14);
-      console.log(this.movie);
+      this.movie = await API.getMovieById(this.id);
     } catch {
       // TODO: alert the user
       alert("movie doesn't exist"); //TODO: replace this alert
@@ -20,10 +19,40 @@ export class MovieDetailsPage extends HTMLElement {
     this.appendChild(content);
     this.querySelector("h2").textContent = this.movie.title;
     this.querySelector("h3").textContent = this.movie.tagline;
+    this.querySelector("img").src = this.movie.poster_url;
+    this.querySelector("#trailer").dataset.url = this.movie.trailer_url;
+    this.querySelector("#overview").textContent = this.movie.overview;
+    this.querySelector("#metadata").innerHTML = `
+<dt>Release Year</dt>
+<dd>${this.movie.release_year}</dd>
+<dt>Score</dt>
+<dd>${this.movie.score} / 10</dd>
+<dt>Popularity</dt>
+<dd>${this.movie.popularity}</dd>
+`;
+
+    const ulGenres = this.querySelector("#genres");
+    ulGenres.innerHTML = "";
+    this.movie.genres.forEach((genre) => {
+      const li = document.createElement("li");
+      li.textContent = genre.name;
+      ulGenres.appendChild(li);
+    });
+
+    const ulCast = this.querySelector("#cast");
+    ulCast.innerHTML = "";
+    this.movie.casting.forEach((actor) => {
+      const li = document.createElement("li");
+      li.innerHTML = `
+                <img src="${actor.image_url ?? "/images/generic_actor.jpg"}" alt="Picture of ${actor.last_name}">
+                <p>${actor.first_name} ${actor.last_name}</p>
+            `;
+      ulCast.appendChild(li);
+    });
   }
 
   connectedCallback() {
-    this.id = 134;
+    this.id = 14;
     this.render();
   }
 }
