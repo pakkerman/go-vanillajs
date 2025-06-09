ae8c68a5be0f23ed4bcd96bc33f7cab5b399c254
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2025-06-09 16:07:32 +0800

    fix: typo

--- models/actor.go
+++ models/actor.go
@@ -4,5 +4,5 @@ type Actor struct {
 	ID        int     `json:"id"`
 	FirstName string  `json:"first_name"`
 	LastName  string  `json:"last_name"`
-	ImageURL  *string `json:"mage_url"` // nullable
+	ImageURL  *string `json:"image_url"` // nullable
 }
