9c9358d63d400a44b7c1e40613b87e8d43a06dfa
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2025-06-13 08:45:35 +0800

    feat: add login page

--- public/services/Routes.js
+++ public/services/Routes.js
@@ -2,10 +2,12 @@ import { HomePage } from "../components/HomePage.js";
 import { MovieDetailsPage } from "../components/MovieDetailsPage.js";
 import { MoviesPage } from "../components/MoviesPage.js";
 import { RegisterPage } from "../components/RegisterPage.js";
+import { LoginPage } from "../components/LoginPage.js";
 
 export const routes = [
   { path: "/", component: HomePage },
   { path: /\/movies\/(\d*)/, component: MovieDetailsPage },
   { path: "/movies", component: MoviesPage }, // search result
   { path: "/account/register", component: RegisterPage }, // search result
+  { path: "/account/login", component: LoginPage }, // search result
 ];
